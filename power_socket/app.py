fdabba000f0659a622360f0938a6eac76d97acad
Nicky Houter <101206236+nickyboy1179@users.noreply.github.com>
2025-05-16 21:07:40 +0200

    Removed SQLite database. Using in memory variables now

--- power_socket/app.py
+++ power_socket/app.py
@@ -1,43 +1,31 @@
 from flask import Flask, render_template, jsonify, request
-import sqlite3
-import threading
-import os
 
 app = Flask(__name__, template_folder='templates')
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DB_PATH = os.path.join(BASE_DIR, 'database', 'database.db')
-DB_LOCK = threading.Lock()
-
+power_state = False
+connected_device = False
 
 def get_power_state_db():
     """Retrieve the power socket's power state from the database."""
-    with DB_LOCK, sqlite3.connect(DB_PATH) as conn:
-        cur = conn.execute("SELECT is_on FROM power_state WHERE id = 1")
-        row = cur.fetchone()
-        return bool(row[0])
+    return power_state
 
 
 def set_power_state_db(on: bool):
     """Set the power socket's power state to the given boolean, persist to database"""
-    with DB_LOCK, sqlite3.connect(DB_PATH) as conn:
-        conn.execute("UPDATE power_state SET is_on = ? WHERE id = 1", (int(on),))
-        conn.commit()
+    global power_state
+    power_state = on
 
 
 def get_connected_device_db():
     """Retrieves whether a device is currently connected to the power socket."""
-    with DB_LOCK, sqlite3.connect(DB_PATH) as conn:
-        curr = conn.execute("SELECT device_connected FROM power_state WHERE id = 1")
-        row = curr.fetchone()
-        return bool(row[0])
+    global connected_device
+    return connected_device
 
 
 def set_connected_device_db(connected: bool):
     """Sets whether a device is currently connected to the power socket."""
-    with DB_LOCK, sqlite3.connect(DB_PATH) as conn:
-        conn.execute("UPDATE power_state SET device_connected = ? WHERE id = 1", (int(connected),))
-        conn.commit()
+    global connected_device
+    connected_device = connected
 
 
 @app.route('/')
--- power_socket/database/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS power_state;
-
-CREATE TABLE power_state (
-    id INTEGER PRIMARY KEY CHECK (id = 1),
-    is_on BOOLEAN NOT NULL,
-    device_connected BOOLEAN NOT NULL,
-    timestamp_conn INTEGER NOT NULL
-);
-INSERT OR IGNORE INTO power_state (id, is_on, device_connected, timestamp_conn) VALUES (1,0,0,0)
