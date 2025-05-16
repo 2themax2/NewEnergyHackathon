01269c3d9f465e2bf417f6fc286b066318424d04
Dn-Dave <davepleijendal@gmail.com>
2025-05-16 12:40:56 +0200

    first api call

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,30 @@
 from flask import Flask
+import requests
+from dotenv import load_dotenv
+import os
 app = Flask(__name__)
 
+
+load_dotenv()
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+
+
+@app.route("/dashboard")
+def hello_there():
+
+
+    url = "https://api.ned.nl/v1/utilizations"
+
+    headers = {
+    'X-AUTH-TOKEN': os.getenv('api'),
+    'accept': 'application/ld+json'}
+    params = {'point': 0, 'type': 2, 'granularity': 3, 'granularitytimezone': 1, 'classification': 2, 'activity': 1,
+    'validfrom[strictly_before]': '2020-11-17', 'validfrom[after]': '2020-11-16'}
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    print(response.text)
+    return str(response)
